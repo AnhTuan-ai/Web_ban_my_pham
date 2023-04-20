0e4b11459f27d802fdd1b91715b1ec32fb65dc09
Tuan <anhtuanhk13t@mail.com>
2023-04-21 02:11:13 +0700

    add su kien cho xem trang xem tat ca san pham , thanh menu doc, xem chi tiet san pham

--- JS/sanpham.js
+++ JS/sanpham.js
@@ -18,7 +18,7 @@ var arrSP =[
         loaichitiet: 'kemduong'
     },
     {
-        TenSP: 'Sữa Rửa Mặt Transino Clear Wash EX< 100g',
+        TenSP: 'Sữa Rửa Mặt Transino Clear Wash EX 100g',
         hinh: '../IMG/sua-rua-mat-transino-clear-wash-ex.jpg',
         thuonghieu: 'Transino',
         gia: '365.000',
@@ -38,24 +38,80 @@ var arrSP =[
 
     
 
+    {
+        TenSP: 'Kem chống nắng The Ordinary Mineral UV Filters SPF 30 with Antioxidants',
+        hinh: '../IMG/The-Ordinary-Mineral-UV-Filters-SPF-30-with-Antioxidants-768x768.jpg',
+        thuonghieu: 'ordinary',
+        gia: '370.000',
+        m_s_k: 'm',
+        loai: 'dactri',
+        loaichitiet: 'chongnang'
+    },
+{
+        TenSP: 'Kem chống nắng The Ordinary Mineral UV Filters SPF 30 with Antioxidants',
+        hinh: '../IMG/The-Ordinary-Mineral-UV-Filters-SPF-30-with-Antioxidants-768x768.jpg',
+        thuonghieu: 'ordinary',
+        gia: '370.000',
+        m_s_k: 'm',
+        loai: 'dactri',
+        loaichitiet: 'chongnang'
+    },
+
 
 ]
 
-function hienSP(){
+function hienSP(arrSP){
     var list = document.getElementById('list');
     list.innerHTML = '';
     for( i = 0; i < arrSP.length; i++){
         m_s_kSP = arrSP[i].m_s_k;
         loaiSP = arrSP[i].loai;
         loaichitietSP = arrSP[i].loaichitiet;
-        list.innerHTML += `<div class = "col-lg-4 col-md-4 col-sm-4 col-xl-4 sp " data-toggle="modal" data-target="#exampleModalCenter">
+        list.innerHTML += `<div class = "col-lg-4 col-md-4 col-sm-4 col-xl-4 sp " data-toggle="modal" data-target="#exampleModalCenter${[i]}">
         <img src = "${arrSP[i].hinh}" width="100%" height="200px">
         <h4> ${arrSP[i].TenSP}</h4>
         <h4> ${arrSP[i].loai}</h4>
         <h4> ${arrSP[i].gia}</h4>
         </div>
+
+        <div class="modal fade" id="exampleModalCenter${[i]}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+          <div class="modal-content">
+            <div class="modal-body">
+              <div class="row">
+                <div class="col-5"><img src = "${arrSP[i].hinh}" width="100%" height="100%"></div>
+                <div class="col-7">
+                <h4> ${arrSP[i].TenSP}</h4>
+                <h4> ${arrSP[i].loai}</h4>
+                <h4> ${arrSP[i].gia}</h4>
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-outline-info">Thêm vào giỏ hàng</button>
+              <button type="button" class="btn btn-outline-danger">Mua với voucher</button>
+            </div>
+          </div>
+        </div>
+      </div>
+        </div>
         `
     }
 }
-hienSP();
+
+document.getElementById("m").addEventListener("click", chonSPmk);
+document.getElementById("s").addEventListener("click", chonSPsk);
+//
+function chonSPmk(){
+    var newArray = arrSP.filter(function(bien){return bien.m_s_k == "m"});
+    hienSP(newArray);
+}
+function chonSPsk(){
+    var newArray = arrSP.filter(function(bien){return bien.m_s_k == "s"});
+    hienSP(newArray);
+}
+hienSP(arrSP);
+// page:
+
+
 
